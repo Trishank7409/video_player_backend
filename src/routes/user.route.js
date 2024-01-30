2369bb715cc27907c05c1522f9b4c0817b00c7f9
Trishank Sharma <trishank7409@gmail.com>
2024-01-30 10:56:27 +0530

    Update user routes to include new features (generated by blackbox.ai )

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,27 +1,49 @@
 import { Router } from "express";
-import { loggedOutUser, loginUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import {
+  changeCurrentPassword,
+  getChannelUserProfile,
+  getCurrentUser,
+  loggedOutUser,
+  loginUser,
+  refreshAccessToken,
+  registerUser,
+  updateAccountDetail,
+  updateCoverImage,
+  updateUserAvatar,
+} from "../controllers/user.controller.js";
 import { upload } from "../middleware/multer.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
-const router=Router()
+const router = Router();
 
 router.route("/registration").post(
-    upload.fields([
-        {
-            name:"avatar",
-            maxCount:1
-        },
-        {
-            name:"coverImage",
-            maxCount:1
-        }
-    ]),
-    registerUser)
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 
+router.route("/login").post(loginUser);
 
+router.route("/logout").post(verifyJWT, loggedOutUser);
+router.route("/refreshToken").post(refreshAccessToken);
+router.route("/changePassword").post(verifyJWT, changeCurrentPassword);
+router.route("/currentUser").get(verifyJWT, getCurrentUser);
+router.route("/updateAccount").patch(verifyJWT, updateAccountDetail);
 
-router.route("/login").post(loginUser)    
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/updateCover")
+  .patch(verifyJWT, upload.single("/coverImage"), updateCoverImage);
+router.route("/c/:username").get(verifyJWT, getChannelUserProfile);
 
-
-router.route("/logout").post(verifyJWT,loggedOutUser)
-router.route("/refreshToken").post(refreshAccessToken)
-export default router
+router.route("/watchHistory").get(verifyJWT, getwatchHistory);
+export default router;
